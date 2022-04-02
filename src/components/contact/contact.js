70945fd534bc5ddf7bd5dac7d5476089ce9d3117
Kellie Harman <knharman@gmail.com>
2022-04-02 14:56:27 -0700

    contact form created

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,7 +3,16 @@ import './contact.css';
 function Contact() {
   return (
     <div className="Contact">
-      <h1>Contact Works</h1>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required/>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required/>
+        <label htmlFor="message">Message:</label>
+        <input type="text" id="message" name="message" required/>
+
+        <input type="submit" id="submit" value="submit" required/>
+      </form>
     </div>
   );
 }
