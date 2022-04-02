0b4e9995a34af8513f06e51143ca78afab95ffa0
Kellie Harman <knharman@gmail.com>
2022-04-02 15:23:04 -0700

    resume added for download

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,9 +1,11 @@
 import './resume.css';
+import {Link} from 'react-router-dom';
+import resume from './resume.pdf';
 
 function Resume() {
   return (
     <div className="Resume">
-      <h1>Resume Works</h1>
+      <Link to={resume} target="_blank" download>Download</Link>
     </div>
   );
 }
