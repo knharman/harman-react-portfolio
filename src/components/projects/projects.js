064e31d3def6da5bb059308a41851ba946352e2d
Kellie Harman <knharman@gmail.com>
2022-04-02 15:36:42 -0700

    screenshots and links for 2 projects added

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,19 +1,22 @@
 import Project from '../project/project';
 import './projects.css';
+import itineraryScreenshot from '../../images/pdxitinerary.png';
+import afkscreenshot from '../../images/afk.png'
+
 
 function Projects() {
   const projects = [
     {
-      title: "First Project",
-      liveLink: "#",
-      githubLink: "#",
-      screenshot: "#"
+      title: "Your Portland Itinerary",
+      liveLink: "https://josephjamescoop.github.io/your-portland-itinerary/",
+      githubLink: "https://github.com/JosephJamesCoop/your-portland-itinerary",
+      screenshot: itineraryScreenshot
     },
     {
-      title: "Second Project",
-      liveLink: "#",
-      githubLink: "#",
-      screenshot: "#"
+      title: "Allergy Free Kitchen",
+      liveLink: "https://afk-network.herokuapp.com/",
+      githubLink: "https://github.com/JosephJamesCoop/the-allergy-free-kitchen",
+      screenshot: afkscreenshot
     },
     {
       title: "Third Project",
