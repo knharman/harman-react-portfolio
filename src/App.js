be27442146be35efb0b1f7483c2e872af57f76d2
Kellie Harman <knharman@gmail.com>
2022-04-02 14:19:35 -0700

    client side routing working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import Header from './components/header/header';
 import Projects from './components/projects/projects';
+import Resume from './components/resume/resume';
+import Contact from './components/contact/contact';
+import About from './components/about/about';
 import Footer from './components/footer/footer';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Projects />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<About />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/resume" element={<Resume />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/projects" element={<Projects />}/>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
