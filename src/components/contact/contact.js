563885210beb745a38f10bb524b186817df743bd
Kellie Harman <knharman@gmail.com>
2022-04-03 14:04:00 -0700

    pug gif added to contacts page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,4 +1,5 @@
 import './contact.css';
+import pug from '../../images/gifs/pug.gif';
 
 function Contact() {
   return (
@@ -9,6 +10,7 @@ function Contact() {
         <textarea className="my-2 p-3" id="message" name="message" placeholder="MESSAGE" required ></textarea>
 
         <input className="p-2" type="submit" id="submit" value="SUBMIT"/>
+        <img src={pug} alt="pug gif" />
       </form>
     </div>
   );
