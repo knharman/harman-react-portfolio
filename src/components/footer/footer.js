8dd53b95abf3c544577c986e1eaf764a4ab3ddbe
Kellie Harman <knharman@gmail.com>
2022-04-02 17:32:07 -0700

    social media icons added to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,26 +1,32 @@
 import './footer.css';
 import Link from '../link/link';
+import gitHubIcon from '../../images/octocat.svg';
+import linkedinIcon from '../../images/linkedin.svg';
+import instagramIcon from '../../images/instagram.svg';
 
 function Footer() {
     const links = [
         {
             title: 'github',
-            url: 'https://github.com/knharman'
+            url: 'https://github.com/knharman',
+            icon: gitHubIcon
         },
         {
             title: 'linkedin',
-            url: 'https://www.linkedin.com/in/kellie-harman/'
+            url: 'https://www.linkedin.com/in/kellie-harman/',
+            icon: linkedinIcon
         },
         {
             title: 'instagram',
-            url: 'https://www.instagram.com/thebluechannel/'
+            url: 'https://www.instagram.com/thebluechannel/',
+            icon: instagramIcon
         }
     ]
     return (
-        <div className="Footer">
+        <div className="Footer d-flex justify-content-center">
             {
                 links.map((link) => {
-                    return <Link title={link.title} url={link.url} key={link.url}/>
+                    return <Link {...link} key={link.url}/>
                 })
             }
         </div>
--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,9 +1,11 @@
 import './link.css';
 
-function Link({ title, url }) {
+function Link({ title, url, icon }) {
   return (
-    <div className="Link">
-      <a href={url} target="_blank" rel="noreferrer">{title}</a>
+    <div className="Link p-5">
+      <a href={url} target="_blank" rel="noreferrer">
+        <img src={icon} alt={title} />
+      </a>
     </div>
   );
 }
