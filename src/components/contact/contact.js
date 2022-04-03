9d9febfa39b515ec0b1e19e172e226fce56fb5f1
Kellie Harman <knharman@gmail.com>
2022-04-03 14:55:04 -0700

    fixing margin issues with pug gif

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,13 +4,14 @@ import pug from '../../images/gifs/pug.gif';
 function Contact() {
   return (
     <div className="Contact">
+      
       <form>
+      <img src={pug} alt="pug gif" />
         <input className="my-2 p-4" type="text" id="name" name="name" required placeholder="NAME"/>
         <input className="my-2 p-4" type="email" id="email" name="email" required placeholder="EMAIL"/>
         <textarea className="my-2 p-3" id="message" name="message" placeholder="MESSAGE" required ></textarea>
 
         <input className="p-2" type="submit" id="submit" value="SUBMIT"/>
-        <img src={pug} alt="pug gif" />
       </form>
     </div>
   );
