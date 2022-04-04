836016904db76220c1c7be8ccd7631e18e713473
Kellie Harman <knharman@gmail.com>
2022-04-03 17:10:32 -0700

    removing build from gitignore

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_git.cb08a5f5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkharman_react_portfolio=self.webpackChunkharman_react_portfolio||[]).push([[2564],{5120:function(e){function t(e){e.languages.git={comment:/^#.*/m,deleted:/^[-\u2013].*/m,inserted:/^\+.*/m,string:/("|')(?:\\.|(?!\1)[^\\\r\n])*\1/m,command:{pattern:/^.*\$ git .*$/m,inside:{parameter:/\s--?\w+/m}},coord:/^@@.*@@$/m,commit_sha1:/^commit \w{40}$/m}}e.exports=t,t.displayName="git",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_git.cb08a5f5.chunk.js.map
---
+++ build/static/js/react-syntax-highlighter_languages_refractor_properties.24334fa0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkharman_react_portfolio=self.webpackChunkharman_react_portfolio||[]).push([[81],{9164:function(t){function a(t){t.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}t.exports=a,a.displayName="properties",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.24334fa0.chunk.js.map
